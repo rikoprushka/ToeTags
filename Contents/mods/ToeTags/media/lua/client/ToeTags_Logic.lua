17558270fb65ca8e7e61917911c33feeff5c0dd0
Orcicorn <orcicorn@gmail.com>
2022-10-02 12:58:49 -0400

    Incorporate ModOptions support

--- Contents/mods/ToeTags/media/lua/client/ToeTags_Logic.lua
+++ Contents/mods/ToeTags/media/lua/client/ToeTags_Logic.lua
@@ -1,3 +1,5 @@
+require "ToeTags_ModOptions";
+
 -- Imported/Modified from lua/client/XpSystem/ISUI/ISCharacterScreen.lua
 -- embedding causes errors, easier to just C/P the one function we need
 local function GetDeadPlayerFavoriteWeapon(player)
@@ -18,66 +20,102 @@ local function GetDeadPlayerFavoriteWeapon(player)
     end
 end
 
--- Create the ToeTag in the Dead Player's Inventory
-local function CreateToeTagOnDeadPlayer(player)
-    -- Only do this for players, and only the player is that playing (mp)
+-- ---------------------------------------------------- --
+-- -- CreateToeTagOnDeadPlayerWithOptions(player) ----- --
+-- --   Populates a toe tag's data and title and places it in the player's
+-- --   inventory when requested.  Fully translated through IG_UI_EN.txt
+-- --   @params     required        IsoCharacter
+-- --   @returns    nothing
+-- ---------------------------------------------------- --
+local function CreateToeTagOnPlayerWithOptions(player)
+    -- Only do it if you're a player character AND the player at the keyboard, not others
     if not instanceof(player, "IsoPlayer") or not player:isLocalPlayer() then return; end;
 
-    -- Gather some information about our dead player
-    local gameTime = getGameTime();
-    local infectedText = nil;
-    if player:getBodyDamage():getInfectionLevel() > 0 then 
-        infectedText = "IGUI_ToeTag_Infected";
+    -- ---------------------------------------------------- --
+    -- Variable Declarations ------------------------------ --
+    -- ---------------------------------------------------- --
+    local ToeTagTitle = "";             -- The Item Name
+    local ToeTagNote = "";              -- The Details / Read list
+
+    -- ---------------------------------------------------- --
+    -- Populate the Toe Tag Name -------------------------- --
+    -- ---------------------------------------------------- --
+    if ToeTagOptions.options.advancedTitle then 
+        ToeTagTitle = getText("IGUI_ToeTag_ItemNameSorted", player:getFullName(), string.format("%08.2f", player:getHoursSurvived()));
     else
-        infectedText = "IGUI_ToeTag_NotInfected";
+        ToeTagTitle = getText("IGUI_ToeTag_ItemName", player:getFullName());
     end
 
-    -- Populate the item Name
-    local itemName = "IGUI_ToeTag_ItemName";
-    if getActivatedMods():contains("ToeTagsSorted") then
-        itemName = "IGUI_ToeTag_ItemNameSorted";
+    -- ---------------------------------------------------- --
+    -- Populate the Toe Tag Details ----------------------- --
+    -- ---------------------------------------------------- --
+    -- Record Username?
+    if ToeTagOptions.options.recordAccount then 
+        ToeTagNote = ToeTagNote .. getText("IGUI_PlayerStats_Username") .. " " .. player:getUsername() .. "\n";
     end
-    itemName = getText(itemName, 
-                       player:getFullName(),
-                       string.format("%08.2f", player:getHoursSurvived())
-               );
+
+    -- Record Profession?
+    if ToeTagOptions.options.recordProfession then
+        local profession = ProfessionFactory.getProfession(player:getDescriptor():getProfession());         
+        if profession then 
+            profession = profession:getName();
+        else
+            profession = getText("UI_FriendState_Unknown");
+        end
+        ToeTagNote = ToeTagNote ..  getText("IGUI_PlayerStats_Profession") .. " " .. profession .. "\n";
+    end
+
+    -- Record Death Date?
+    if ToeTagOptions.options.recordDeathDate then
+        ToeTagNote = ToeTagNote .. getText("IGUI_ToeTag_Died", getGameTime():getCalender():getTime()) .. "\n";
+    end
+
+    -- Record Survival Time?
+    if ToeTagOptions.options.recordSurvived then
+        ToeTagNote = ToeTagNote .. getText("IGUI_char_Survived_For") .. ": " .. player:getTimeSurvived() .. "\n";
+    end
+
+    -- Record Favorite Weapon?
+    if ToeTagOptions.options.recordFavWeapon then
+        ToeTagNote = ToeTagNote .. getText("IGUI_char_Favourite_Weapon") .. ": " .. GetDeadPlayerFavoriteWeapon(player) .. "\n";
+    end
+
+    -- Record Kill Count?
+    if ToeTagOptions.options.recordKillCount then
+        ToeTagNote = ToeTagNote .. getText("IGUI_char_Zombies_Killed") .. ": " .. player:getZombieKills() .. "\n";
+    end
     
-    -- Request the human-readable title for the profession through Profession Factory
-    -- If you don't use Profession factory, you get the StringID of the profession (e.g.: fisherman)
-    -- which then can be loaded through the Profession Factory to get the proper translated profession (e.g.: Pescador [Spanish])
-    -- In the event of an unknown or incorrect ID, just use the "Unknown" translation
-    local profession = ProfessionFactory.getProfession(player:getDescriptor():getProfession());
-    if profession then 
-        profession = profession:getName();
-    else
-        profession = getText("UI_FriendState_Unknown");
+    -- Record Infected ?
+    if ToeTagOptions.options.recordInfected then 
+        if player:getBodyDamage():getInfectionLevel() > 0 then
+            ToeTagNote = ToeTagNote .. getText("IGUI_ToeTag_Infected") .. "\n";
+        else
+            ToeTagNote = ToeTagNote .. getText("IGUI_ToeTag_NotInfected") .. "\n";
+        end
     end
 
-    -- Prepare the Toe Tag Details
-    local details = getText("IGUI_PlayerStats_Username") .. " " .. player:getUsername() .. "\n" .. 
-                    getText("IGUI_PlayerStats_Profession") .. " " .. profession .. "\n" ..
-                    getText("IGUI_ToeTag_Died", gameTime:getCalender():getTime()) .. "\n" .. 
-                    "\n" .. 
-                    getText("IGUI_char_Favourite_Weapon") .. ": " .. GetDeadPlayerFavoriteWeapon(player) .. "\n" .. 
-                    getText("IGUI_char_Survived_For") .. ": " .. player:getTimeSurvived() .. "\n" ..
-                    getText("IGUI_char_Zombies_Killed") .. ": " .. player:getZombieKills() .. "\n" .. 
-                    "\n" ..
-                    getText(infectedText)
-                ;
+    -- ---------------------------------------------------- --
+    -- If all options are disabled, just write "Deceased" - --
+    -- ---------------------------------------------------- --
+    if ToeTagNote == "" then ToeTagNote = getText("IGUI_health_Deceased"); end
 
+    -- ---------------------------------------------------- --
     -- Add Toe Tag to the player inventory and write the data
+    -- Lock the page with a non-existant account (can not edit)
+    -- ---------------------------------------------------- --
     local item = player:getInventory():AddItem("Base.ToeTag");
-    item:setName(itemName)
-    item:addPage(1, details)
-
-    -- Lock the page with a non-existant account so that you can not edit the data
-    -- nor use it as a blank piece of paper. Toe tags are technically just paper notes
+    item:setName(ToeTagTitle)
+    item:addPage(1, ToeTagNote)
     item:setLockedBy("GrimReaper#" .. getGametimeTimestamp());
 end
-Events.OnCharacterDeath.Add(CreateToeTagOnDeadPlayer)
 
--- -------------------------------------------------- --
--- -- DEBUG ----------------------------------------- --
--- -------------------------------------------------- --
--- Events.OnWeaponSwing.Add(CreateToeTagOnDeadPlayer)
--- -------------------------------------------------- --
+-- ---------------------------------------------------- --
+-- -- ON DEATH ADD TOE TAG ---------------------------- --
+-- ---------------------------------------------------- --
+Events.OnCharacterDeath.Add(CreateToeTagOnPlayerWithOptions)
+
+-- ---------------------------------------------------- --
+-- -- DEBUG ------------------------------------------- --
+-- ---------------------------------------------------- --
+-- Events.OnWeaponSwing.Add(CreateToeTagOnPlayerWithOptions)
+-- ---------------------------------------------------- --
---
+++ Contents/mods/ToeTags/media/lua/client/ToeTags_ModOptions.lua
@@ -0,0 +1,67 @@
+-- Default Configurations for ModOptions
+local ToeTagSettings = {
+    options_data = {
+        advancedTitle = {
+            name = "IGUI_ToeTag_Options_AdvancedTitle",
+            tooltip = "IGUI_ToeTag_Options_AdvancedTitle_Tooltip",
+            default = false,
+        },
+        recordAccount = {
+            name = "IGUI_ToeTag_Options_RecordAccount",
+            tooltip = "IGUI_ToeTag_Options_RecordAccount_Tooltip",
+            default = false,
+        },
+        recordProfession = {
+            name = "IGUI_ToeTag_Options_RecordProfession",
+            tooltip = "IGUI_ToeTag_Options_RecordProfession_Tooltip",
+            default = true,
+        },
+        recordDeathDate = {
+            name = "IGUI_ToeTag_Options_RecordDeathDate",
+            tooltip = "IGUI_ToeTag_Options_RecordDeathDate_Tooltip",
+            default = true,
+        },
+        recordSurvived = {
+            name = "IGUI_ToeTag_Options_RecordSurvived",
+            tooltip = "IGUI_ToeTag_Options_RecordSurvived_Tooltip",
+            default = true,
+        },
+        recordFavWeapon = {
+            name = "IGUI_ToeTag_Options_RecordFavWeapon",
+            tooltip = "IGUI_ToeTag_Options_RecordFavWeapon_Tooltip",
+            default = true,
+        },
+        recordKillCount = {
+            name = "IGUI_ToeTag_Options_RecordKillCount",
+            tooltip = "IGUI_ToeTag_Options_RecordKillCount_Tooltip",
+            default = true,
+        },
+        recordInfected = {
+            name = "IGUI_ToeTag_Options_RecordInfection",
+            tooltip = "IGUI_ToeTag_Options_RecordInfection_Tooltip",
+            default = true,
+        }
+    },
+    mod_id = "ToeTags",
+    mod_shortname = getText("IGUI_ToeTag_Options_NameShort"),
+    mod_fullname = getText("IGUI_ToeTag_Options_NameLong"),
+};
+
+-- If ModOptions is installed, LOAD USER'S SETTINGS
+-- Otherwise if no ModOptions, use my defaults
+if ModOptions and ModOptions.getInstance then
+    ToeTagOptions = ModOptions:getInstance(ToeTagSettings);
+else
+    ToeTagOptions = {
+        options = {
+            advancedTitle = false,
+            recordAccount = false,
+            recordProfession = true,
+            recordDeathDate = true,
+            recordFavWeapon = true,
+            recordKillCount = true,
+            recordSurvived = true,
+            recordInfected = true,
+        }
+    };
+end
